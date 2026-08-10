81741d70128d09ec6d0b29f1d0e1c7d873cf6270
Cody Waits <137742107+cwaits6@users.noreply.github.com>
2026-08-10 06:18:53 -0400

    fix: correct output text and drop release-please config to re-bootstrap (#25)

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ var version = "0.10.0" // x-release-please-version
 
 func main() {
 	fmt.Printf("workflow-test/v2 version: %s\n", version)
-	fmt.Println("second feature")
+	fmt.Println("second feature (patched)")
 }
 
 func unused() string { return "sticky release-as probe" }
